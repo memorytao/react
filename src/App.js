920f2b2840c6931339646681eea1a27ec6c4f1a7
Chinawat <memorytao@gmail.com>
2020-07-15 17:54:19 +0700

    " add bootsterab "

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Spinner from 'react-bootstrap/Spinner';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a>}
+       
+      </header> */
+      }
+      <>
+        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="secondary" />
+        <Spinner animation="border" variant="success" />
+        <Spinner animation="border" variant="danger" />
+        <Spinner animation="border" variant="warning" />
+        <Spinner animation="border" variant="info" />
+        <Spinner animation="border" variant="light" />
+        <Spinner animation="border" variant="dark" />
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+        <Spinner animation="grow" variant="danger" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="info" />
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="grow" variant="dark" />
+      </>
     </div>
   );
 }
