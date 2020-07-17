1ad0a8fc132cb85b930ed4ba26cdbba17121d9b5
Chinawat <memorytao@gmail.com>
2020-07-17 10:34:01 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -11,22 +11,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {<a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>}
-       
-      </header> */
-        }
+        <div>MODE {process.env.NODE_ENV} | {process.env.PUBLIC_URL} </div>
         <MainUI />
       </div>
     );
